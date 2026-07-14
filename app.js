7001dbdf58f9ca5286f92a91d50e0f8cd1e3a6c8
atul8660 <lokhandeatul909@gmail.com>
2026-07-14 21:51:02 +0530

    Fix: redirect root route to listings

--- app.js
+++ app.js
@@ -65,9 +65,9 @@ const sessionOptions = {
   },
 };
 
-// app.get("/", (req, res) => {
-//   res.send("I am root");
-// });
+ app.get("/", (req, res) => {
+   res.send("I am root");
+ });
 
 app.use(session(sessionOptions));
 app.use(flash());
